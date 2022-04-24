9c459cf4500699821aed8ec15d495bc7fdc17d9a
liubei <liubei@ezgrid.cn>
2022-04-24 17:07:08 +0800

    verify调用次数时，多个单元测试互不影响

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -26,6 +26,26 @@ public class UnitTest1
         result.ShouldBe("Result:3");
 
         // 验证是否调用了一次
-        mock.Verify(x=> x.Add(1,2), Times.Once);
+        mock.Verify(x => x.Add(1, 2), Times.Once);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+
+        // given 
+        var mock = new Mock<IMath>();
+        mock.Setup(x => x.Add(3, 5)).Returns(8);
+
+        var c2 = new Class2(mock.Object);
+
+        // when
+        var result = c2.XX(3, 5);
+
+        // then
+        result.ShouldBe("Result:8");
+
+        // 验证是否调用了一次（两个单元测试之间互不影响）
+        mock.Verify(x => x.Add(3, 5), Times.Once);
     }
 }
